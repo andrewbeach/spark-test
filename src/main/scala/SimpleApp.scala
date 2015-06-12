3d698aa7d76faff59ccd281259a62936b3d72884
Andrew Beach <andrew@team.shoeboxed.com>
2015-06-12 15:19:08 -0400

    added tf-idf and kmeans clustering

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -3,10 +3,10 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.Tokenizer
+
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.feature.IDF
 
@@ -17,22 +17,34 @@ object SimpleApp {
 		val conf = new SparkConf().setAppName("Simple Application")
 		val sc = new SparkContext(conf)
 
-		val tokenizer = new Tokenizer()
-
 		// Create pair RDD of Product Name and word sequence
-		val products: RDD[(String, Seq[String])] = sc.textFile("data/products_simple.txt").map(line => (line, line.toLowerCase.split(" ").toSeq))
+		val products: RDD[(String, Seq[String])] = sc.textFile(productFile).map(line => (line, line.toLowerCase.split(" ").toSeq))
 		
 		// Create pair RDD of Product Name and hashed term-frequency vector
-		val hashingTF = new HashingTF()
+		val hashingTF = new HashingTF(numFeatures = 10000)
 		val tf: RDD[(String, Vector)] = products.mapValues(hashingTF.transform)
 		tf.cache()
 		
 		// Create pair RDD of Product Name and tf-idf vector
 		val idf = new IDF().fit(tf.values)
 		val tfidf: RDD[(String, Vector)] = tf.mapValues(idf.transform)
+		tfidf.values.cache()
+
+		// Cluster the data into classes using KMeans
+		val kmeans = new KMeans().setK(200)
+		val clusters: KMeansModel = kmeans.run(tfidf.values)
 
 		// Print 
-		tfidf.foreach(println)
+		val product_points: RDD[(String, Int)] = tfidf.mapValues(clusters.predict)
+
+		val product_clusters: RDD[(Int, String)] = product_points.map(_.swap)
+
+		val grouped_products = product_clusters.groupByKey()
+
+		grouped_products.foreach(println)
+
+
+
 	}
 
 	// def tokenize_text(line: String) {
